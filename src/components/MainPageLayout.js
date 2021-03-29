e8aafb59a8bf59649cdc432bbebd6fd3a9631698
sushmit <sushmitpatil1@gmail.com>
2021-03-29 21:48:01 +0530

    Added Layout managment and created title

---
+++ src/components/MainPageLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navs from './Navs';
+import Title from './Title';
+
+const MainPageLayout = ({ children }) => {
+  return (
+    <div>
+      <Title
+        title="Box Office"
+        subtitle="Are You Looking For a Movie or an Actor?"
+      />
+      <Navs />
+
+      {children}
+    </div>
+  );
+};
+
+export default MainPageLayout;
